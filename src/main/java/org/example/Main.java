d85201f2a979a1e8f4bf813c2546ff605ae35216
Nikola Zaharkin <nikolay.zaharkin1@gmail.com>
2022-10-04 21:18:07 +0300

    commit 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,18 +6,20 @@ import org.example.knight.MyException;
 import org.example.knight.inventory.Inventory;
 import org.example.knight.inventory.weapon.Sword;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Shop shop = new Shop();
-//        System.out.println("print\n");
-//        shop.printProducts();
-//        Inventory inventory = shop.getProduct(3);
-//        System.out.println("get inventory" + inventory.toString());
-//        shop.printProducts();
+        System.out.println("print\n");
+        shop.printProducts();
+        Inventory inventory = shop.getProduct(3);
+        System.out.println("get inventory" + inventory.toString());
+        shop.printProducts();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите товар от 1-го до 6-ти");
+
         Knight knight1 = new Knight("Рыцарь1", 100, 100);
-        for(int i = 0;i< shop.inv.size();i++){
-            knight1.buy(shop.inv.get(i));
-        }
-        System.out.println();
+
     }
 }
